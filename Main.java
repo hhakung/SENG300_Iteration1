9cc6ec29675397cbb69186cb68331f97c32937d5
DESKTOP-3T992KK\Esther <hhakung@gmail.com>
2018-03-13 14:57:00 -0600

    formatted things nicely

--- Main.java
+++ Main.java
@@ -4,107 +4,90 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.*;
 import java.io.*;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-
+import org.eclipse.jdt.core.dom.*;
 
 public class Main {
 	public static void main (String[] args) throws FileNotFoundException {
-	/*	for (String s: args) {
-			System.out.println(s);
-		}*/
-			Path path = Paths.get(args[0]);
-			System.out.println("Path: "+ path);
+		Path path = Paths.get(args[0]);
+		System.out.println("Path: "+ path);
 
-			String textPath = args[0];
-			System.out.println("textPath:"+textPath);
+		String textPath = args[0];
+		System.out.println("textPath:"+textPath);
 
-			String type = args[1];
-			System.out.println("Type: "+type);
+		String type = args[1];
+		System.out.println("Type: "+type);
 
-			Main m = new Main();
-			m.listFiles(textPath);
+		Main m = new Main();
+		m.listFiles(textPath);
 
-			File[] files = m.finder(textPath);
+		File[] files = m.finder(textPath);
 
-			//Prints out all the java files
-			for (File f: files){
-					System.out.println("Java Files: "+ f.getName());
+		//Prints out all the java files
+		for (File f: files) {
+			System.out.println("Java Files: "+ f.getName());
 
+			String fileInString = f.toString();
 
-					String fileInString = f.toString();
+			Scanner scanner = new Scanner(new File(fileInString));
+			String theString = "";
 
-					Scanner scanner = new Scanner(new File(fileInString));
-					String theString = "";
+			theString = scanner.nextLine();
+			while (scanner.hasNextLine()) {
+				theString = theString /*+ "\n" */+ scanner.nextLine();
+			}
 
-					theString = scanner.nextLine();
-					while (scanner.hasNextLine()) {
-						theString = theString /*+ "\n" */+ scanner.nextLine();
-					}
+			char[] charArray = theString.toCharArray();
 
-					char[] charArray = theString.toCharArray();
+			for (char c: charArray) {
+					System.out.println(c);
+			}
 
-					for (char c: charArray){
-						System.out.println(c);
-					}
-
-					//Call parsing method to parse each CompilationUnit
-					m.parse(charArray);
-
-		  }
-
-
-
+			//Call parsing method to parse each CompilationUnit
+			m.parse(charArray);
+		}
 	}
 
 	/**
-		* List all the files under a directory
-		* @param directoryName to be listed
-		*/
-	 public void listFiles(String directoryName){
-			 File directory = new File(directoryName);
-			 //get all the files from a directory
-			 File[] fList = directory.listFiles();
-
-
-			 for (File file : fList){
-						if (file.isFile()){
-							 System.out.println(file.getName());
-
-						}
-			 }
+	* List all the files under a directory
+	* @param directoryName to be listed
+	*/
+	public void listFiles(String directoryName) {
+		 File directory = new File(directoryName);
+			 
+		 //get all the files from a directory
+		 File[] fList = directory.listFiles();
+		 
+		 for (File file : fList) {
+			 if (file.isFile()) {
+				 System.out.println(file.getName());
+			}
 		}
+	}
 
-		public File[] finder( String directoryName){
-        File dir = new File(directoryName);
+	public File[] finder( String directoryName) {
+		File dir = new File(directoryName);
 
         return dir.listFiles(new FilenameFilter() {
-                 public boolean accept(File dir, String filename)
-                      { return filename.endsWith(".java"); }
-        } );
-
+        	public boolean accept(File dir, String filename) { 
+        		return filename.endsWith(".java"); 
+        	}
+        });
     }
 
-		public void parse(char[] sourceCode) {
-			System.out.println("IN PARSE METHOD");
-			ASTParser parser = ASTParser.newParser(AST.JLS8);
+	public void parse(char[] sourceCode) {
+		System.out.println("IN PARSE METHOD");
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
 
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			parser.setSource(sourceCode);
-			parser.setResolveBindings(true);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(sourceCode);
+		parser.setResolveBindings(true);
 
-			CompilationUnit cu = (CompilationUnit)parser.createAST(null);
+		CompilationUnit cu = (CompilationUnit)parser.createAST(null);
 			
 			
-			TypeVisitor v = new TypeVisitor();
-			System.out.println("created a new instance of TypeVisitor");
-			cu.accept(v);
-			System.out.println("finished visiting");
-			
-
-		}
-
-
+		TypeVisitor v = new TypeVisitor();
+		System.out.println("created a new instance of TypeVisitor");
+		cu.accept(v);
+		System.out.println("finished visiting");
+	}
 }
